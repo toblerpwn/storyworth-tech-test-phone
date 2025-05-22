d6494977d1ff8d33e35139fc22619aea0784bcf7
toblerpwn <toblerpwn@gmail.com>
2025-05-22 14:14:50 -0700

    🚸 add pause at beginning of phone call

--- services/twilio.ts
+++ services/twilio.ts
@@ -27,7 +27,7 @@ export const createTwilioCall = async (phoneNumber: string) => {
     From: TWILIO_PHONE_NUMBER,
     To: phoneNumber,
     Twiml:
-      '<Response><Say voice="woman">Hello! This is Storyworth, where every story is worth sharing. Please begin recording your one minute story now...</Say><Record maxLength="60" playBeep="true" /></Response>',
+      '<Response><Pause length="2"/><Say voice="woman">Hello! This is Storyworth, where every story is worth sharing. Please begin recording your one minute story now...</Say><Record maxLength="60" playBeep="true" /></Response>',
     Record: "true",
   });
 
