302e96a3a76bbb8e241e9f3509f815f0e225ec17
toblerpwn <toblerpwn@gmail.com>
2025-05-23 14:25:37 -0700

    💬 update call status text

--- app/components/ui/brandedButton.tsx
+++ app/components/ui/brandedButton.tsx
@@ -15,7 +15,7 @@ export const BrandedButton = ({
     <button
       disabled={disabled}
       type="button"
-      className={`bg-[#07949D] text-white font-label uppercase font-bold tracking-[0.125em] rounded-full p-[12px] w-[185] shadow-[0px_4px_16px_rgba(0,0,0,0.10)] disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+      className={`bg-[#07949D] text-white font-label uppercase font-bold tracking-[0.125em] rounded-full p-[12px] w-[185] max-h-12 shadow-[0px_4px_16px_rgba(0,0,0,0.10)] disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
       onClick={onClick}
     >
       {label}
